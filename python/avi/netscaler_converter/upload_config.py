5ab6a30b61131f269ef6d2bf87638a1eccf5dfa6
Ramesh Tathe <ramesh.tathe@avinetworks.com>
2017-03-02 17:15:56 +0530

    Changes Include: 1. Fixes timeout issue in auto upload config by calculating timeout based on pool size.

--- python/avi/netscaler_converter/upload_config.py
+++ python/avi/netscaler_converter/upload_config.py
@@ -1,5 +1,6 @@
 from avi.sdk.avi_api import ApiSession
 import logging
+import sys
 
 LOG = logging.getLogger(__name__)
 
@@ -10,9 +11,12 @@ def upload_config_to_controller(avi_config_dict, controller_ip,
     session = ApiSession.get_session(controller_ip, username,
                                      password=password, tenant=tenant)
     try:
+        time_out = len(avi_config_dict["Pool"])/5
+        if time_out < 300:
+            time_out = 300
         d = {'configuration': avi_config_dict}
         path = 'configuration/import'
-        resp = session.post(path, data=d)
+        resp = session.post(path, data=d, timeout=time_out)
         if resp.status_code < 300:
             LOG.info("Config uploaded to controller successfully")
         else:
